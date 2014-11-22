0f2dfaa1e3b503d48d71acb39b0539a603f052c5
Kris <krismolendyke+github@gmail.com>
2014-11-22 18:04:00 -0500

    Greatly simplify things by using format-time-string.

--- clock-face.el
+++ clock-face.el
@@ -2,47 +2,32 @@
 
 ;;; Commentary:
 
-;; The most "helpful" function here inserts at point the Unicode clock
-;; face character corresponding to the current hour.
-
 ;;; Code:
 
 (provide 'clock-face)
 
-(defvar clock-face-number-to-str
-  '((0 . "🕛")
-    (1 . "🕐")
-    (2 . "🕑")
-    (3 . "🕒")
-    (4 . "🕓")
-    (5 . "🕔")
-    (6 . "🕕")
-    (7 . "🕖")
-    (8 . "🕗")
-    (9 . "🕘")
-    (10 . "🕙")
-    (11 . "🕚"))
-  "Mapping of numeric hour index to Unicode character string.")
+(defvar clock-face--hour->face
+  '(("01" . "🕐")
+    ("02" . "🕑")
+    ("03" . "🕒")
+    ("04" . "🕓")
+    ("05" . "🕔")
+    ("06" . "🕕")
+    ("07" . "🕖")
+    ("08" . "🕗")
+    ("09" . "🕘")
+    ("10" . "🕙")
+    ("11" . "🕚")
+    ("12" . "🕛"))
+  "Mapping of zero-padded hour to Unicode character string.")
 
-(defun clock-face--hour->index (hour)
-  "Convert HOUR to an index."
-  (if (> hour 11) (- hour 12) hour))
-
-(defun clock-face--index (time-string)
-  "Get a numeric hour index from TIME-STRING.
-The index can be used to lookup a clock face string in
-`clock-face-number-to-str'."
-  (clock-face--hour->index
-   (string-to-number
-    (car (split-string (nth 3 (split-string time-string)) ":")))))
-
-(defun clock-face--str (hour)
-  "Get a clock face Unicode character string for HOUR."
-  (cdr (assoc (clock-face--hour->index hour) clock-face-number-to-str)))
+(defun clock-face--get (hour)
+  "Get the clock face for the given hour."
+  (cdr (assoc hour clock-face--hour->face)))
 
 (defun clock-face-current ()
   "Get the current clock face character string."
-  (clock-face--str (clock-face--index (current-time-string))))
+  (clock-face--get (format-time-string "%I")))
 
 (defun clock-face-insert-current ()
   "Insert the current clock face character string."
